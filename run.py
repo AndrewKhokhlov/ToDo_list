fd43aea3962db03a73f46dc2b32689461305493f
AndrewLee13 <andrew.copri@icloud.com>
2025-07-09 01:00:55 +0300

    Измаенения ран

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
-from app import create_app, db
+import os
+from app import create_app
 
 app = create_app()
 
-# Авто‑создание таблиц при каждом запуске (если их ещё нет)
-with app.app_context():
-    db.create_all()
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
